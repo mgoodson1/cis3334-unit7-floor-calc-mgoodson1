5f85c0819a10a9060dff7073e779819561ff7550
mgoodson <darth123>
2018-03-06 15:19:09 -0600

    try again: res\layout directory not checked for some reason : 2017-03-05 Matt Goodson : 3334-700: Assignment 7 - Flooring Activity - Additional code to return results to main_activity plus comments

--- app/src/main/java/edu/css/mgoodson1/cis_3334_unit_7_floor_calc_mgoodson1/DisplayResultActivity.java
+++ app/src/main/java/edu/css/mgoodson1/cis_3334_unit_7_floor_calc_mgoodson1/DisplayResultActivity.java
@@ -1,13 +1,50 @@
 package edu.css.mgoodson1.cis_3334_unit_7_floor_calc_mgoodson1;
 
+/**
+ * Project: CIS 3334-700 - Unit 7: Assignment 7 - Flooring Activity
+ * Developer: Matt Goodson
+ * Verison: 2018-03-05
+ *
+ * App with two activities(screens) used to calculate the amount of flooring needed
+ *
+ * Second Activity used to calculate and display the Area
+ *
+ */
+
+
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class DisplayResultActivity extends AppCompatActivity {
+
+    TextView tvArea;
+    Double width;
+    Double length;
+    Double area;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_result);
+
+        tvArea = (TextView) findViewById(R.id.textViewArea);
+
+        Bundle extras = getIntent().getExtras();
+        width = extras.getDouble("Width");
+        length = extras.getDouble("Length");
+
+        area = width * length;
+        tvArea.setText(area.toString());
     }
+
+    public void finish(){
+            Intent intent = new Intent();
+            intent.putExtra("Area", area.toString());
+            setResult(RESULT_OK, intent);
+            super.finish();
+
+    }
+
 }
